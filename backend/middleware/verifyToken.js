d52b23be57220c19da400e728cdfe590c03bc7e5
Shahna-20 <shahnaak123@gmail.com>
2025-04-26 14:52:03 +0530

    get all user except logged in userapi route

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -9,7 +9,7 @@ export const isAuthenticate = async (req , res , next) => {
             return next(errorHandler(401, "Unauthorized"))
         }
 
-   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+   jwt.verify(token,process.env.JWT_SECRET, (err, user) => {
     if(err){
         return next(errorHandler(403, "Forbidden"))
     }
